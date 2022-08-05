00511b1b8b5775b6ede867191904632c42a1ff66
rootryan649 <110697404+rootryan649@users.noreply.github.com>
2022-08-05 22:16:36 +0100

    Main source code file creation

---
+++ game.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    char welcomeMessage[] = "Welcome to game\n";
+    printf(welcomeMessage);
+    int location;
+    int command;
+    printf("Enter command: ");
+    scanf("%d", &command);
+    if (command == 1){
+        printf("Enter location: ");
+        scanf("%d", &location);
+        if(location == 1){
+            printf("You are in the living room");
+        }  
+        else if (location == 2){
+            printf("You are in the bedroom");
+        }
+        else if (location == 3){
+            printf("You are in the kitchen");
+        }
+        else if (location == 4){
+            printf("You are in the bathroom");
+        } 
+        else{
+            printf("Invalid location");
+        }
+    }
+    else{
+        printf("Invalid location");
+    }
+    return 0;
+}
